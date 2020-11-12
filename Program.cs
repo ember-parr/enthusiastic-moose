583bb730711ffd5d09bc8d31a249493a2f522ad8
Ember Parr <ember.devops@gmail.com>
2020-11-12 15:55:03 -0600

    phase six complete

--- Program.cs
+++ Program.cs
@@ -13,7 +13,14 @@ namespace EnthusiasticMoose
             MooseSays("H I, I'M  E N T H U S I A S T I C !");
             MooseSays("I really am enthusiastic");
             bool isTrue = MooseAsks("Is Canada real?");
-            Console.WriteLine(isTrue);
+            if (isTrue)
+            {
+                MooseSays("Really? It seems very unlikely.");
+            }
+            else
+            {
+                MooseSays("I  K N E W  I T !!!");
+            }
         }
 
         static void MooseSays(string message)
