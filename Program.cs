1ff5e53189ede4ae1b197539ca0e660c3602fef2
Ember Parr <ember.devops@gmail.com>
2020-11-12 16:01:11 -0600

    phase seven complete

--- Program.cs
+++ Program.cs
@@ -12,15 +12,10 @@ namespace EnthusiasticMoose
 
             MooseSays("H I, I'M  E N T H U S I A S T I C !");
             MooseSays("I really am enthusiastic");
-            bool isTrue = MooseAsks("Is Canada real?");
-            if (isTrue)
-            {
-                MooseSays("Really? It seems very unlikely.");
-            }
-            else
-            {
-                MooseSays("I  K N E W  I T !!!");
-            }
+            CanadaQuestion();
+            EnthusiasticQuestion();
+            LoveCSharpQuestion();
+            SecretQuestion();
         }
 
         static void MooseSays(string message)
@@ -73,6 +68,61 @@ namespace EnthusiasticMoose
             else
             {
                 return false;
+            }
+        }
+
+
+        static void CanadaQuestion()
+        {
+            bool isTrue = MooseAsks("Is Canada real?");
+            if (isTrue)
+            {
+                MooseSays("Really? It seems very unlikely.");
+            }
+            else
+            {
+                MooseSays("I  K N E W  I T !!!");
+            }
+        }
+
+
+
+        static void EnthusiasticQuestion()
+        {
+            bool isEnthusiastic = MooseAsks("Are you enthusiastic?");
+            if (isEnthusiastic)
+            {
+                MooseSays("Yay!");
+            }
+            else
+            {
+                MooseSays("You should try it!");
+            }
+        }
+
+        static void LoveCSharpQuestion()
+        {
+            bool doesLoveCSharp = MooseAsks("Do you love C# yet?");
+            if (doesLoveCSharp)
+            {
+                MooseSays("Good job sucking up to your instructor!");
+            }
+            else
+            {
+                MooseSays("You will...oh, yes, you will...");
+            }
+        }
+
+        static void SecretQuestion()
+        {
+            bool isEverythingFine = MooseAsks("Do you want to know a secret?");
+            if (isEverythingFine)
+            {
+                MooseSays("ME TOO!!!! I love secrets...tell me one!");
+            }
+            else
+            {
+                MooseSays("Oh, no...secrets are the best, I love to share them!");
             }
         }
     }
