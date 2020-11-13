12ca457e4cbccd78750e3da7627213e1ef7035c0
Ember Parr <ember.devops@gmail.com>
2020-11-12 18:19:17 -0600

    advanced challenge one complete

--- Program.cs
+++ Program.cs
@@ -10,40 +10,20 @@ namespace EnthusiasticMoose
             Console.WriteLine("--------------------------------------------");
             Console.WriteLine();
 
-            MooseSays("H I, I'M  E N T H U S I A S T I C !");
-            MooseSays("I really am enthusiastic");
+            // MooseSays("H I, I'M  E N T H U S I A S T I C !");
+            // MooseSays("I really am enthusiastic");
             Questions();
         }
 
         static void MooseSays(string message)
         {
             Console.WriteLine($@"
-                                       _.--^^^--,
-                                    .'          `\
-  .-^^^^^^-.                      .'              |
- /          '.                   /            .-._/
-|             `.                |             |
- \              \          .-._ |          _   \
-  `^^'-.         \_.-.     \   `          ( \__/
-        |             )     '=.       .,   \
-       /             (         \     /  \  /
-     /`               `\        |   /    `'
-     '..-`\        _.-. `\ _.__/   .=.
-          |  _    / \  '.-`    `-.'  /
-          \_/ |  |   './ _     _  \.'
-               '-'    | /       \ |
-                      |  .-. .-.  |
-                      \ / o| |o \ /
-                       |   / \   |    {message}
-                      / `^`   `^` \
-                     /             \
-                    | '._.'         \
-                    |  /             |
-                     \ |             |
-                      ||    _    _   /
-                      /|\  (_\  /_) /
-                      \ \'._  ` '_.'
-                       `^^` `^^^`
+   |\---/|
+   | ,_, |
+    \_`_/-..----.
+ ___/ `   ' ,""+ \          {message}
+(__...'   __\    |`.___.';
+  (_,...'(_,.`__)/'.....+
             ");
         }
 
@@ -71,46 +51,147 @@ namespace EnthusiasticMoose
 
         static void Questions()
         {
-            bool isTrue = MooseAsks("Is Canada real?");
-            if (isTrue)
+
+            Random r = new Random();
+            int getNumber = r.Next(1, 20);
+
+
+            // bool isTrue = MooseAsks("Are dogs real?");
+            // if (isTrue)
+            // {
+            //     MooseSays("Really? It seems very unlikely.");
+            // }
+            // else
+            // {
+            //     MooseSays("I  K N E W  I T !!!");
+            // }
+
+            // bool isEnthusiastic = MooseAsks("Do You Like Catnip?");
+            // if (isEnthusiastic)
+            // {
+            //     MooseSays("Yay!");
+            // }
+            // else
+            // {
+            //     MooseSays("You should try it!");
+            // }
+
+            // bool doesLoveCSharp = MooseAsks("Do you love C# yet?");
+            // if (doesLoveCSharp)
+            // {
+            //     MooseSays("Good job sucking up to your instructor!");
+            // }
+            // else
+            // {
+            //     MooseSays("You will...oh, yes, you will...");
+            // }
+
+            // bool isEverythingFine = MooseAsks("Do you want to know a secret?");
+            // if (isEverythingFine)
+            // {
+            //     MooseSays("ME TOO!!!! I love secrets...tell me one!");
+            // }
+            // else
+            // {
+            //     MooseSays("Oh, no...secrets are the best, I love to share them!");
+            // }
+
+            bool wantToSeeMagic = MooseAsks("Do you want to shake the magic cat ball?");
+            if (wantToSeeMagic)
             {
-                MooseSays("Really? It seems very unlikely.");
+                MagicCat(getNumber);
             }
             else
             {
-                MooseSays("I  K N E W  I T !!!");
+                MooseSays("Well fine then... rude!");
             }
 
-            bool isEnthusiastic = MooseAsks("Are you enthusiastic?");
-            if (isEnthusiastic)
-            {
-                MooseSays("Yay!");
-            }
-            else
-            {
-                MooseSays("You should try it!");
-            }
 
-            bool doesLoveCSharp = MooseAsks("Do you love C# yet?");
-            if (doesLoveCSharp)
-            {
-                MooseSays("Good job sucking up to your instructor!");
-            }
-            else
-            {
-                MooseSays("You will...oh, yes, you will...");
-            }
+        }
 
-            bool isEverythingFine = MooseAsks("Do you want to know a secret?");
-            if (isEverythingFine)
-            {
-                MooseSays("ME TOO!!!! I love secrets...tell me one!");
-            }
-            else
-            {
-                MooseSays("Oh, no...secrets are the best, I love to share them!");
-            }
 
+        static void MagicCat(int randomNumber)
+        {
+            if (randomNumber == 1)
+            {
+                MooseSays("As I see it, yes.");
+            }
+            if (randomNumber == 2)
+            {
+                MooseSays("Ask again later.");
+            }
+            if (randomNumber == 3)
+            {
+                MooseSays("Better not tell you now");
+            }
+            if (randomNumber == 4)
+            {
+                MooseSays("Cannot predict now");
+            }
+            if (randomNumber == 5)
+            {
+                MooseSays("Concentrate and ask again");
+            }
+            if (randomNumber == 6)
+            {
+                MooseSays("Don't count on it");
+            }
+            if (randomNumber == 7)
+            {
+                MooseSays("It is certain");
+            }
+            if (randomNumber == 8)
+            {
+                MooseSays("It is decidedly so");
+            }
+            if (randomNumber == 9)
+            {
+                MooseSays("Most likely");
+            }
+            if (randomNumber == 10)
+            {
+                MooseSays("My reply is no");
+            }
+            if (randomNumber == 11)
+            {
+                MooseSays("My sources say no");
+            }
+            if (randomNumber == 12)
+            {
+                MooseSays("Outlook not so good");
+            }
+            if (randomNumber == 13)
+            {
+                MooseSays("Outlook good");
+            }
+            if (randomNumber == 14)
+            {
+                MooseSays("Reply hazy, try again");
+            }
+            if (randomNumber == 15)
+            {
+                MooseSays("signs point to yes");
+            }
+            if (randomNumber == 16)
+            {
+                MooseSays("very doubtful");
+            }
+            if (randomNumber == 17)
+            {
+                MooseSays("Yes!");
+            }
+            if (randomNumber == 18)
+            {
+                MooseSays("Without a doubt");
+            }
+            if (randomNumber == 19)
+            {
+                MooseSays("Yes - Definitely");
+            }
+            if (randomNumber == 20)
+            {
+                MooseSays("You may rely on it");
+            }
 
         }
     }
